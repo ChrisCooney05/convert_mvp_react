260b2ca808b798158a07892fbaab14e2cafdedc6
Chris Cooney <chris.cooney05@hotmail.co.uk>
2020-07-22 16:38:10 +0100

    moved everything into react

---
+++ src/player.js
@@ -0,0 +1,79 @@
+export default class Player {
+  constructor(gameHeight, gameWidth) {
+    // game area size
+    this.gameHeight = gameHeight;
+    this.gameWidth = gameWidth;
+    // object size
+    this.height = 30;
+    this.width = 30;
+    // object position
+    this.position = {
+      x: 0,
+      y: this.gameHeight - this.height,
+    };
+    //movement value
+    this.moveIncrement = {
+      x: 30,
+      y: 30,
+    };
+    // object velocaity
+    this.vel = {
+      x: 0,
+      y: 0,
+    };
+    // speed
+    this.speed = 10;
+    // friction
+    this.friction = 1 - this.speed / this.moveIncrement.x;
+    // move interval (ms)
+    this.moveInterval = 1000;
+  }
+
+  draw(ctx) {
+    ctx.fillRect(this.position.x, this.position.y, this.width, this.height);
+  }
+
+  update(deltaTime) {
+    if (!deltaTime) return;
+
+    //console.log(this.position.x)
+
+    this.position.x += this.vel.x;
+    this.vel.x *= this.friction;
+    // can add a final position object that gets updated in the moveRight/Left function
+    // if block to check if you are in final position
+    // if true then set vel = 0
+  }
+
+  moveRight() {
+    //this.position.x += this.movement.x;
+    this.vel.x = this.speed;
+  }
+
+  moveLeft() {
+    //this.position.x -= this.movement.x;
+    this.vel.x = -this.speed;
+  }
+
+  // placeholder function
+  start(mockData) {
+    let i = 0;
+    let interval = setInterval(() => {
+      this._stringToFunction(mockData[i]);
+      i++;
+      if (i === mockData.length) clearInterval(interval);
+    }, this.moveInterval);
+  }
+
+  _stringToFunction(input) {
+    // eslint-disable-next-line default-case
+    switch (input) {
+      case "player.moveRight()":
+        this.moveRight();
+        break;
+      case "player.moveLeft()":
+        this.moveLeft();
+        break;
+    }
+  }
+}
